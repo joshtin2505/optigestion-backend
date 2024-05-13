e9acde09fe70838af44cf82db64f89155ec5f94f
joshtin2505 <justincastro2505@gmail.com>
2024-05-13 16:32:22 -0500

    Add

--- src/main/java/com/example/App/initializer/DataInitializer.java
+++ src/main/java/com/example/App/initializer/DataInitializer.java
@@ -2,20 +2,21 @@ package com.example.App.initializer;
 
 import com.example.App.entities.Estado;
 import com.example.App.entities.Rol;
+import com.example.App.entities.TipoRequerimiento;
 import com.example.App.services.EstadoService;
 import com.example.App.services.RolService;
+import com.example.App.services.TipoRequerimientoService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final RolService rolService;
-    private EstadoService estadoService;
-
-    public DataInitializer(RolService rolService) {
-        this.rolService = rolService;
-    }
+    private final EstadoService estadoService;
+    private final TipoRequerimientoService tipoRequerimientoService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -34,14 +35,14 @@ public class DataInitializer implements CommandLineRunner {
 
         // ------ReqStates------
 
-        Estado estado1 = new Estado(0, "papelera");
-        Estado estado2 = new Estado(1,"borrador");
-        Estado estado3 = new Estado(2, "archivado");
-        Estado estado4 = new Estado(3, "enviado");
-        Estado estado5 = new Estado(4, "aprobado");
-        Estado estado6 = new Estado(5, "rechazado");
-        Estado estado7 = new Estado(6, "cotizado");
-        Estado estado8 = new Estado(7, "para comprar");
+        Estado estado1 = new Estado(0L, "papelera");
+        Estado estado2 = new Estado(1L,"borrador");
+        Estado estado3 = new Estado(2L, "archivado");
+        Estado estado4 = new Estado(3L, "enviado");
+        Estado estado5 = new Estado(4L, "aprobado");
+        Estado estado6 = new Estado(5L, "rechazado");
+        Estado estado7 = new Estado(6L, "cotizado");
+        Estado estado8 = new Estado(7L, "para comprar");
 
         existOrSaveEstado(estado1);
         existOrSaveEstado(estado2);
@@ -51,16 +52,31 @@ public class DataInitializer implements CommandLineRunner {
         existOrSaveEstado(estado6);
         existOrSaveEstado(estado7);
         existOrSaveEstado(estado8);
+        // ------reqTypes------
+
+        TipoRequerimiento tipoRequerimiento1 = new TipoRequerimiento(0L, "Servicio", "Requerimiento para la compra de servicios") ;
+        TipoRequerimiento tipoRequerimiento2 = new TipoRequerimiento(1L, "Producto", "Requerimiento para la compra de productos");
+        TipoRequerimiento tipoRequerimiento3 = new TipoRequerimiento(2L, "Varios", "Requerimiento para la compra de varios productos o servicios");
+
+        existOrSaveTipoRequerimiento(tipoRequerimiento1);
+        existOrSaveTipoRequerimiento(tipoRequerimiento2);
+        existOrSaveTipoRequerimiento(tipoRequerimiento3);
     }
 
-    public void existOrSaveRol(Rol rol){
+
+    private void existOrSaveRol(Rol rol){
         if(rolService.existsById(rol.getId_rol())){
             rolService.guardarRol(rol);
         }
     }
-    public void existOrSaveEstado(Estado estado){
-        if(estadoService.existsById(estado.getId_estado())){
+    private void existOrSaveEstado(Estado estado){
+        if(estadoService.existsById(estado.getId())){
             estadoService.guardarEstado(estado);
+        }
+    }
+    private void existOrSaveTipoRequerimiento(TipoRequerimiento tipoRequerimiento) {
+        if (tipoRequerimientoService.existsById(tipoRequerimiento.getId_tipo_requerimiento())) {
+            tipoRequerimientoService.guardarTipoRequerimiento(tipoRequerimiento);
         }
     }
 }
